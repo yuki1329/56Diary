3144c1444b2cc2e364fbb09279b6ed3fbcd9c650
Yuki Takahatake <yuki12152124@gmail.com>
2019-11-14 11:49:59 +0800

    Revert "11/14"
    This reverts commit d50033c4833d983c6c5e2f228a5312ca7bbb6bbb.

--- Diary/app/Http/Requests/CreateDiary.php
+++ Diary/app/Http/Requests/CreateDiary.php
@@ -29,12 +29,4 @@ class CreateDiary extends FormRequest
             'body' => 'required'
         ];
     }
-    // エラー文言を表示する際の、name属性と表示名の設定をする
-    public function attributes()
-    {
-        return [
-            'title' => 'タイトル',
-            'body' => '本文'
-        ];
-    }
 }
