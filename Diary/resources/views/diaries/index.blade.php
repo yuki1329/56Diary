f612fd1da12488abfcbe966ba438f4ad62294d26
Yuki Takahatake <yuki12152124@gmail.com>
2019-11-14 21:33:50 +0800

    変更

--- Diary/resources/views/diaries/index.blade.php
+++ Diary/resources/views/diaries/index.blade.php
@@ -1,14 +1,22 @@
+@extends('layout')
 
-<!-- layout.blade.phpを読み込む -->
-  @extends('layout')
-  @section('title')
-  一覧
-  @endsection
+@section('title', '一覧')
 
+@section('content')
+  <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">新規投稿</a>
+   @foreach ($diaries as $diary)
+   <div class="m-4 p-4 border border-primary">
+   <p>{{$diary->title}}</p>
+   <p>{{$diary->body}}</p>
+   <p>{{$diary->created_at}}</p>
+<!-- //delete -->
+<form action="{{ route('diary.destroy',['id' => $diary->id]) }}" method="POST" class="d-inline">
+@csrf
+@method('delete')
+<button class="btn btn-danger">削除</button>
+</form>
 
-    @section('content')
+   </div>
+   @endforeach
 
-    <a href="{{route('diary.create')}}" class="btn btn-primary btn-block">新規投稿</a>
-    @foreach($diaries as $diary)
-    @endforeach
-    @endsection
+   @endsection
