3a7f3f3aa23d5e39d453d59c4828dd49ec6b9620
Yuki Takahatake <yuki12152124@gmail.com>
2019-11-22 09:17:35 +0800

    変更

--- Diary/public/js/diary.js
+++ Diary/public/js/diary.js
@@ -1,45 +1,102 @@
-$(document).on('click','.js-like', function(){
-    //いいねされた日記のIDを取得
-    //$(this) : 今回クリックされたiタグ(ハートマーク)
-    // .sibligs('XXX') : 兄弟要素を取得する
-    // .val() : inputタグのvalueの値を取得する
-    let diaryId=$(this).siblings('.diary-id').val();
-
-    //likeメソッドの実行
-    like(diaryId,$(this));
-});
-    //diaryId:いいねする日記のID
-    //clickedBtn:今回クリックされたいいねのアイコン
-function like(diaryId ,clickedBtn){
-    $.ajax({
-        url:'diary/' + diaryId + '/like',
-        type: 'POST',
-        // CSRF対策のため、tokenを送信する
-        headers:{
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    }).done((data) =>{
-        console.log(data);
-        //いいねの数を増やす
-        //1.現在のいいね数を取得
-        //text() : <a>XXX</a> XXXの部分を取得
-        let num =clickedBtn.siblings('.js-like-num').text();
-
-        //numを数値に変換する
-        num=Number(num);
-
-        //2.1プラスした結果を設定する
-        //text(YYY) : <a>XXX</a> XXXの部分をYYYに置き換える
-        clickedBtn.siblings('.js-like-num').text(num +1);
-        //いいねのボタンのデザインを変更
-        changeLikeBtn(clickedBtn);
-    }).fail((error) =>{
-        console.log(error);
+$(document).on('click','.js-like',function(){
+    ​
+                //いいねされた日記のIDを取得したい
+                //$(this)　：今回クリックされたiタグ（ハートマーク）
+                //.siblings('xxx') :兄弟要素を取得する
+                //val(); inputタグのvalueお値を取得する。
+                let diaryId =$(this).siblings('.diary-id').val();
+    ​
+                //likeメソッドを実行
+                like(diaryId,$(this));
+    ​
     });
-}
-
-    //btn:色を変えたい、いいねアイコン
-function changeLikeBtn(btn){
-    btn.toggleClass('far');
-    btn.toggleClass('fas');
-}
+    ​
+                //diaryId:いいねのする日記のID
+                //clickedBtn: 今回クリックされたいいねアイコン
+                function like(diaryId,clickedBtn){
+                                                // ?にはIの数字が入るーーー？から置き換わる
+                    $.ajax({
+                            url:'diary/'+ diaryId +'/like',
+                            type:'POST',
+                            dataType:'json',
+                            //CSRF対策の為、tokenを送信する.
+                            headers:{
+                                'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+                            }
+                    }).done((data) => {
+                        console.log(data);
+                        //いいねの数を増やそう
+                        //1,現在のいいね数を取得
+                        //text(): <a>XXX</a>XXXの部分を取得
+                        let num = clickedBtn.siblings('.js-like-num').text();
+    ​
+    ​
+                        //numを数値に変換する
+                        num =Number(num);
+    ​
+                        //２、１プラスした結果を設定する
+                        //text(YYY); <a>XXX</a>XXXの部分をYYYに置き換える
+                        clickedBtn.siblings('.js-like-num').text(num + 1);
+    ​
+    ​
+                        //いいねのボタンの「デザイン」を変更
+                        changeLikeBtn(clickedBtn);
+    ​
+    ​
+                    }).fail((error) => {
+                        console.log(error);
+                    });
+    ​
+    }
+    ​
+                    //btn ：には色を変えたい、いいねアイコン。toggleClass〜があれば追加〜なければ削除
+                    function changeLikeBtn(btn){
+                        btn.toggleClass('far').toggleClass('fas');
+                        btn.toggleClass('js-like').toggleClass('js-dislike');
+                        //btn.toggleClass('far').toggleClass('fas');;;;;; も上の２行と同じ(メソッドチェーンと言うjqueryだけ)
+                    }
+    ​
+                    //いいね解除の処理
+                    $(document).on('click','.js-dislike',function(){
+                        //いいね解除された日記のIDの取得
+                        let diaryId =$(this).siblings('.diary-id').val();
+    ​
+                        //dislikeのメソッドの実行.
+                        dislike(diaryId,$(this));
+                    });
+    ​
+                    function dislike(diaryId,clickedBtn){
+                        // ?にはIの数字が入るーーー？から置き換わる
+                        $.ajax({
+                        url:'diary/'+ diaryId +'/dislike',
+                        type:'POST',
+                        dataType:'json',
+                        // dataType:'json',
+                        //CSRF対策の為、tokenを送信する.
+                        headers:{
+                            'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+                        }
+                    }).done((data) => {
+                        console.log(data);
+                        //いいねの数を増やそう
+                        //1,現在のいいね数を取得
+                        //text(): <a>XXX</a>XXXの部分を取得
+                        let num = clickedBtn.siblings('.js-like-num').text();
+    ​
+    ​
+                        //numを数値に変換する
+                        num =Number(num);
+    ​
+                        //２、１マイナスした結果を設定する
+                        //text(YYY); <a>XXX</a>XXXの部分をYYYに置き換える
+                        clickedBtn.siblings('.js-like-num').text(num - 1);
+    ​
+    ​
+                        //いいねのボタンの「デザイン」を変更
+                        changeLikeBtn(clickedBtn);
+    ​
+    ​
+                        }).fail((error) => {
+                        console.log(error);
+                        });
+                    }
--- Diary/resources/views/diaries/index.blade.php
+++ Diary/resources/views/diaries/index.blade.php
@@ -1,33 +1,41 @@
 @extends('layouts.app')
-
 @section('title', '一覧')
-
 @section('content')
-  <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">新規投稿</a>
-   @foreach ($diaries as $diary)
-   <div class="m-4 p-4 border border-primary">
-   <p>{{$diary->title}}</p>
-   <p>{{$diary->body}}</p>
-   <p>{{$diary->created_at}}</p>
-   {{-- Auth::check() : ログインしていたらtrue,他はfalse --}}
-   @if (Auth::check() && $diary->user_id == Auth::user()->id)
-
-   {{-- edit --}}
-   <a href="{{route('diary.edit',['id' =>$diary->id])}}"  class="btn btn-success">編集</a>
-<!-- //delete -->
-   <form action="{{ route('diary.destroy',['id' => $diary->id]) }}" method="POST" class="d-inline">
-   @csrf
-   @method('delete')
-   <button class="btn btn-danger">削除</button>
-</form>
-
-   @endif
-   <div class="mt-3 ml-3">
-      <i class="far fa-heart fa-lg text-danger js-like"></i>
-   <input type="hidden" class="diary-id" value="{{ $diary->id }}">
-      <span class="js-like-num">200</span>
-   </div>
-   </div>
-   @endforeach
-
-   @endsection
+  <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
+    新規投稿
+  </a>
+  @foreach ($diaries as $diary)
+    <div class="m-4 p-4 border border-primary">
+      <p>{{$diary->title}}</p>
+      <p>{{$diary->body}}</p>
+      <p>{{$diary->created_at}}</p>
+​
+        {{-- @if (Auth::check()) ログインしていたらtrue,他はfalseにlaravelが設定してくれている--}}
+        @if (Auth::check() && $diary->user_id ==  Auth::user()->id)
+            <a href="{{ route('diary.edit', ['id'=>$diary->id])}}" class="btn btn-success">編集</a>
+​
+            <form action="{{ route('diary.destroy', ['id' => $diary->id ]) }}" method="POST" class="d-inline">
+                  @csrf
+                  @method('delete')
+                  <button class="btn btn-danger">削除</button>
+            </form>
+        @endif
+        <div class="mt-3 ml-3">
+          @if (Auth::check() && $diary->likes->contains(function($user){
+            return $user->id ===Auth::user()->id;
+          }))
+              {{-- //ログインしている且つこの日記にいいねをしている場合 --}}
+              <i class="fas fa-heart fa-lg text-danger js-dislike"></i>
+​
+          @else
+              <i class="far fa-heart fa-lg text-danger js-like"></i>
+​
+          @endif
+​
+            <input type="hidden" class="diary-id" value="{{ $diary->id }}">
+            <span class="js-like-num">{{ $diary->likes->count() }}</span>
+        </div>
+​
+    </div>
+  @endforeach
+@endsection
